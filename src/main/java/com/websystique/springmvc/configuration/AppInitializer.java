79e49635d27273faff84f45e2bbd216c2e8b62cc
david mukungi <mukungidavid@gmail.com>
2018-12-17 12:05:54 +0300

    added services

---
+++ src/main/java/com/websystique/springmvc/configuration/AppInitializer.java
@@ -0,0 +1,45 @@
+package com.websystique.springmvc.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+ 
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+ 
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		registration.setMultipartConfig(getMultipartConfigElement());
+	}
+	private MultipartConfigElement getMultipartConfigElement(){
+		MultipartConfigElement multipartConfigElement = new MultipartConfigElement(LOCATION, MAX_FILE_SIZE, MAX_REQUEST_SIZE, FILE_SIZE_THRESHOLD);
+		return multipartConfigElement;
+	}
+
+    /*Set these variables for your project needs*/
+
+	//	private static final String LOCATION = "C:/mytemp/";   for windows i'll use C:/templ
+	private static final String LOCATION = "";
+
+	private static final long MAX_FILE_SIZE = 1024 * 1024 * 25;//25MB
+
+	private static final long MAX_REQUEST_SIZE = 1024 * 1024 * 30;//30MB
+
+	private static final int FILE_SIZE_THRESHOLD = 0;
+}
+
---
+++ src/main/java/com/websystique/springmvc/controller/AppController.java
@@ -0,0 +1,322 @@
+package com.websystique.springmvc.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.websystique.springmvc.model.FileBucket;
+import com.websystique.springmvc.model.UserDocument;
+import com.websystique.springmvc.service.UserDocumentService;
+import com.websystique.springmvc.util.FileValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import com.websystique.springmvc.model.User;
+import com.websystique.springmvc.model.UserProfile;
+import com.websystique.springmvc.service.UserProfileService;
+import com.websystique.springmvc.service.UserService;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+@RequestMapping("/")
+@SessionAttributes("roles")
+public class AppController {
+
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	UserProfileService userProfileService;
+
+	@Autowired
+	MessageSource messageSource;
+
+	@Autowired
+	PersistentTokenBasedRememberMeServices persistentTokenBasedRememberMeServices;
+	
+	@Autowired
+	AuthenticationTrustResolver authenticationTrustResolver;
+
+
+	@Autowired
+	UserDocumentService userDocumentService;
+
+	@Autowired
+	FileValidator fileValidator;
+
+	@InitBinder("fileBucket")
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(fileValidator);
+	}
+
+
+	/**
+	 * This method will list all existing users.
+	 */
+	@RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
+	public String listUsers(ModelMap model) {
+
+		List<User> users = userService.findAllUsers();
+		model.addAttribute("users", users);
+		model.addAttribute("loggedinuser", getPrincipal());
+		return "userslist";
+	}
+
+	/**
+	 * This method will provide the medium to add a new user.
+	 */
+	@RequestMapping(value = { "/newuser" }, method = RequestMethod.GET)
+	public String newUser(ModelMap model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		model.addAttribute("edit", false);
+		model.addAttribute("loggedinuser", getPrincipal());
+		return "registration";
+	}
+
+	/**
+	 * This method will be called on form submission, handling POST request for
+	 * saving user in database. It also validates the user input
+	 */
+	@RequestMapping(value = { "/newuser" }, method = RequestMethod.POST)
+	public String saveUser(@Valid User user, BindingResult result,
+			ModelMap model) {
+
+		if (result.hasErrors()) {
+			return "registration";
+		}
+
+		/*
+		 * Preferred way to achieve uniqueness of field [sso] should be implementing custom @Unique annotation 
+		 * and applying it on field [sso] of Model class [User].
+		 * 
+		 * Below mentioned peace of code [if block] is to demonstrate that you can fill custom errors outside the validation
+		 * framework as well while still using internationalized messages.
+		 * 
+		 */
+		if(!userService.isUserSSOUnique(user.getId(), user.getSsoId())){
+			FieldError ssoError =new FieldError("user","ssoId",messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
+		    result.addError(ssoError);
+			return "registration";
+		}
+		
+		userService.saveUser(user);
+
+		model.addAttribute("success", "User " + user.getFirstName() + " "+ user.getLastName() + " registered successfully");
+		model.addAttribute("loggedinuser", getPrincipal());
+		//return "success";
+		return "registrationsuccess";
+	}
+
+
+	/**
+	 * This method will provide the medium to update an existing user.
+	 */
+	@RequestMapping(value = { "/edit-user-{ssoId}" }, method = RequestMethod.GET)
+	public String editUser(@PathVariable String ssoId, ModelMap model) {
+		User user = userService.findBySSO(ssoId);
+		model.addAttribute("user", user);
+		model.addAttribute("edit", true);
+		model.addAttribute("loggedinuser", getPrincipal());
+		return "registration";
+	}
+	
+	/**
+	 * This method will be called on form submission, handling POST request for
+	 * updating user in database. It also validates the user input
+	 */
+	@RequestMapping(value = { "/edit-user-{ssoId}" }, method = RequestMethod.POST)
+	public String updateUser(@Valid User user, BindingResult result,
+			ModelMap model, @PathVariable String ssoId) {
+
+		if (result.hasErrors()) {
+			return "registration";
+		}
+
+		/*//Uncomment below 'if block' if you WANT TO ALLOW UPDATING SSO_ID in UI which is a unique key to a User.
+		if(!userService.isUserSSOUnique(user.getId(), user.getSsoId())){
+			FieldError ssoError =new FieldError("user","ssoId",messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
+		    result.addError(ssoError);
+			return "registration";
+		}*/
+
+
+		userService.updateUser(user);
+
+		model.addAttribute("success", "User " + user.getFirstName() + " "+ user.getLastName() + " updated successfully");
+		model.addAttribute("loggedinuser", getPrincipal());
+		return "registrationsuccess";
+	}
+
+	
+	/**
+	 * This method will delete an user by it's SSOID value.
+	 */
+	@RequestMapping(value = { "/delete-user-{ssoId}" }, method = RequestMethod.GET)
+	public String deleteUser(@PathVariable String ssoId) {
+		userService.deleteUserBySSO(ssoId);
+		return "redirect:/list";
+	}
+	
+
+	/**
+	 * This method will provide UserProfile list to views
+	 */
+	@ModelAttribute("roles")
+	public List<UserProfile> initializeProfiles() {
+		return userProfileService.findAll();
+	}
+	
+	/**
+	 * This method handles Access-Denied redirect.
+	 */
+	@RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+	public String accessDeniedPage(ModelMap model) {
+		model.addAttribute("loggedinuser", getPrincipal());
+		return "accessDenied";
+	}
+
+	/**
+	 * This method handles login GET requests.
+	 * If users is already logged-in and tries to goto login page again, will be redirected to list page.
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginPage() {
+		if (isCurrentAuthenticationAnonymous()) {
+			return "login";
+	    } else {
+	    	return "redirect:/list";  
+	    }
+	}
+
+	/**
+	 * This method handles logout requests.
+	 * Toggle the handlers if you are RememberMe functionality is useless in your app.
+	 */
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){    
+			//new SecurityContextLogoutHandler().logout(request, response, auth);
+			persistentTokenBasedRememberMeServices.logout(request, response, auth);
+			SecurityContextHolder.getContext().setAuthentication(null);
+		}
+		return "redirect:/login?logout";
+	}
+
+	/**
+	 * This method returns the principal[user-name] of logged-in user.
+	 */
+	private String getPrincipal(){
+		String userName = null;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+			userName = ((UserDetails)principal).getUsername();
+		} else {
+			userName = principal.toString();
+		}
+		return userName;
+	}
+	
+	/**
+	 * This method returns true if users is already authenticated [logged-in], else false.
+	 */
+	private boolean isCurrentAuthenticationAnonymous() {
+	    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	    return authenticationTrustResolver.isAnonymous(authentication);
+	}
+
+
+	@RequestMapping(value = { "/add-document-{userId}" }, method = RequestMethod.GET)
+	public String addDocuments(@PathVariable int userId, ModelMap model) {
+		User user = userService.findById(userId);
+		model.addAttribute("user", user);
+
+		FileBucket fileModel = new FileBucket();
+		model.addAttribute("fileBucket", fileModel);
+
+		List<UserDocument> documents = userDocumentService.findAllByUserId(userId);
+		model.addAttribute("documents", documents);
+
+		return "managedocuments";
+	}
+
+
+	@RequestMapping(value = { "/download-document-{userId}-{docId}" }, method = RequestMethod.GET)
+	public String downloadDocument(@PathVariable int userId, @PathVariable int docId, HttpServletResponse response) throws IOException {
+		UserDocument document = userDocumentService.findById(docId);
+		response.setContentType(document.getType());
+		response.setContentLength(document.getContent().length);
+		response.setHeader("Content-Disposition","attachment; filename=\"" + document.getName() +"\"");
+
+		FileCopyUtils.copy(document.getContent(), response.getOutputStream());
+
+		return "redirect:/add-document-"+userId;
+	}
+
+	@RequestMapping(value = { "/delete-document-{userId}-{docId}" }, method = RequestMethod.GET)
+	public String deleteDocument(@PathVariable int userId, @PathVariable int docId) {
+		userDocumentService.deleteById(docId);
+		return "redirect:/add-document-"+userId;
+	}
+
+	@RequestMapping(value = { "/add-document-{userId}" }, method = RequestMethod.POST)
+	public String uploadDocument(@Valid FileBucket fileBucket, BindingResult result, ModelMap model, @PathVariable int userId) throws IOException{
+
+		if (result.hasErrors()) {
+			System.out.println("validation errors");
+			User user = userService.findById(userId);
+			model.addAttribute("user", user);
+
+			List<UserDocument> documents = userDocumentService.findAllByUserId(userId);
+			model.addAttribute("documents", documents);
+
+			return "managedocuments";
+		} else {
+
+			System.out.println("Fetching file");
+
+			User user = userService.findById(userId);
+			model.addAttribute("user", user);
+
+			saveDocument(fileBucket, user);
+
+			return "redirect:/add-document-"+userId;
+		}
+	}
+
+	private void saveDocument(FileBucket fileBucket, User user) throws IOException{
+
+		UserDocument document = new UserDocument();
+
+		MultipartFile multipartFile = fileBucket.getFile();
+
+		document.setName(multipartFile.getOriginalFilename());
+		document.setDescription(fileBucket.getDescription());
+		document.setType(multipartFile.getContentType());
+		document.setContent(multipartFile.getBytes());
+		document.setUser(user);
+		userDocumentService.saveDocument(document);
+	}
+
+
+}
